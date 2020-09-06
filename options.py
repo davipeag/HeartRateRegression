b3c1ed1010e69c0172f31b360ce61d409467a183
Davi Pedrosa de Aguiar <daviaguiar@outlook.com>
2020-09-06 18:20:43 -0300

    using colab validation

---
+++ options.py
@@ -0,0 +1,90 @@
+
+
+#%%
+
+from default_utils import (
+    DefaultPamapPreprocessing,
+    FcPamapPreprocessing,
+    make_deep_conv_lstm,
+    make_cnn_imu2,
+    make_fcnn,
+    make_our_conv_lstm,
+    make_attention_transormer_model,
+    TrainOurConvLSTM,
+    TrainXY
+)
+
+from preprocessing_utils import (
+    ShuffleIS,
+    OurConvLstmToAttentionFormat,
+    OurConvLstmToCnnImuFormat
+)
+
+from models_utils import (
+    OurConvLstmDataset,
+    DatasetXY
+)
+
+
+preprocessing_options = {
+      "OurConvLSTM": DefaultPamapPreprocessing(ts_count = 300, donwsampling_ratio = 1),
+      "AttentionTransformer": DefaultPamapPreprocessing(
+        ts_count = 300, donwsampling_ratio = 1, last_transformer=OurConvLstmToAttentionFormat()),
+      "DeepConvLSTM": DefaultPamapPreprocessing(last_transformer=OurConvLstmToCnnImuFormat()),
+      "CnnIMU": DefaultPamapPreprocessing(last_transformer=OurConvLstmToCnnImuFormat()),
+      "FCNN": FcPamapPreprocessing(),
+      "NoISOurConvLSTM": DefaultPamapPreprocessing(ts_count = 300, donwsampling_ratio = 1),
+      "ShuffleISOurConvLSTM": DefaultPamapPreprocessing(ts_count = 300, donwsampling_ratio = 1, last_transformer=ShuffleIS),
+      "LstmISOurConvLSTM": DefaultPamapPreprocessing(ts_count = 300, donwsampling_ratio = 1)
+}
+
+
+dataset_cls_options = {
+      "OurConvLSTM": OurConvLstmDataset,
+      "AttentionTransformer": DatasetXY,
+      "DeepConvLSTM": DatasetXY,
+      "CnnIMU": DatasetXY,
+      "FCNN": DatasetXY,
+      "NoISOurConvLSTM": OurConvLstmDataset,
+      "ShuffleISOurConvLSTM": OurConvLstmDataset,
+      "LstmISOurConvLSTM": OurConvLstmDataset,
+  }
+
+
+net_options = {
+      "OurConvLSTM": lambda t=162,r=160 : make_our_conv_lstm(40,1),
+      "AttentionTransformer": lambda t=162,r=160: make_attention_transormer_model(args["device"]),
+      "DeepConvLSTM": lambda t=162,r=160 : make_deep_conv_lstm(total_size=t, recursive_size=4),
+      "CnnIMU": lambda t=162,r=160 : make_cnn_imu2(total_size=t, recursive_size=4),
+      "FCNN": lambda t=162,r=160 : make_fcnn(),
+      "NoISOurConvLSTM":lambda t=162,r=160: make_our_conv_lstm(40,1,"nois"),
+      "ShuffleISOurConvLSTM": lambda t=162,r=160 : make_our_conv_lstm(40,1,"regular"),
+      "LstmISOurConvLSTM": lambda t=162,r=160: make_our_conv_lstm(40,1, "lstmis"),
+  }
+
+
+
+trainer_options = {
+      "OurConvLSTM": lambda pars : TrainOurConvLSTM(**pars),
+      "AttentionTransformer": lambda pars: TrainXY(
+          **pars, get_last_y_from_x = lambda x: x[:,1, 0, -1].reshape(-1,1)
+      ),
+      "CnnIMU":lambda pars: TrainXY(
+          **pars,
+          get_last_y_from_x = lambda x: np.mean(x[:,0,200:300, 0], axis=1).reshape(-1,1)
+      ),
+      "DeepConvLSTM":lambda pars : TrainXY(
+          **pars,
+          get_last_y_from_x = lambda x: np.mean(x[:,0,200:300, 0], axis=1).reshape(-1,1)
+      ),
+      "FCNN":lambda pars: TrainXY(
+          **pars,
+          get_last_y_from_x = lambda x: x[:,0, 0].reshape(-1,1)
+      ),
+      "NoISOurConvLSTM":lambda pars: TrainOurConvLSTM(**pars),
+
+      "ShuffleISOurConvLSTM": lambda pars: TrainOurConvLSTM(**pars),
+
+      "LstmISOurConvLSTM": lambda pars: TrainOurConvLSTM(**pars),
+  }
+
--- preprocessing_utils.py
+++ preprocessing_utils.py
@@ -137,6 +137,8 @@ class ZTransformer( BaseEstimator, TransformerMixin ):
         16.1190996 , 19.92196213,  1.18436304,  5.71159398,  6.83934918,
         3.55699737,  5.346378  ,  6.38219302,  3.19114986,  1.07355547,
         0.59660246,  1.84255171, 18.87860076, 21.61180986, 20.30858045])
+    
+    self.transformer.n_features_in_ = 40
   
   def fit(self, df, y=None):
     # arr = df[self.column_names].to_numpy()
