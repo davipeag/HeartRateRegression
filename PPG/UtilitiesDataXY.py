46e8f0098552e9eead21d083b04f711a8192b015
Davi Pedrosa de Aguiar <daviaguiar@outlook.com>
2020-10-23 14:32:38 -0300

    try

--- PPG/UtilitiesDataXY.py
+++ PPG/UtilitiesDataXY.py
@@ -145,7 +145,12 @@ class JointTrValDataLoaderFactory():
          
     
     def split(self, xys, train_ratio =0.8):
-        xy_tr, xy_val = zip(*[(self.split_array(v, ratio = train_ratio)) for v in xys])
+        xy_tr, xy_val = [],[]
+        for e in xys:
+            t,v = self.split_array(e, train_ratio)
+            xy_tr.append(t)
+            xy_val.append(v)
+        #xy_tr, xy_val = zip(*[(self.split_array(v, ratio = train_ratio)) for v in xys])
         return xy_tr, xy_val
   
     def make_loader(self, xys, shuffle, batch_size):
