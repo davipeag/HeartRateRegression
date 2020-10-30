488bdc0e1fa524ebcb854dd53f2ff20ea7aaf416
Davi Pedrosa de Aguiar <daviaguiar@outlook.com>
2020-10-30 10:30:02 -0300

    standardizing training epochs

--- PPG/FullTrainer.py
+++ PPG/FullTrainer.py
@@ -379,7 +379,7 @@ class IeeeJointValNoHrPceLstmFullTrainer():
         train_helper = TrainHelperIS(
             epoch_trainer, loader_tr, loader_val, loader_ts, metrics_comuter.mae)
 
-        metric = train_helper.train(200)
+        metric = train_helper.train(40)
 
         p = [metrics_comuter.inverse_transform_label(v)
              for v in epoch_trainer.evaluate(loader_ts)[-2:]]
