606a875f352a9bc570b09f2d55b29a4e8ad975fc
Davi Pedrosa de Aguiar <daviaguiar@outlook.com>
2020-11-11 18:51:59 -0300

    using mse criterion

--- PPG/FullTrainer.py
+++ PPG/FullTrainer.py
@@ -111,8 +111,8 @@ class JointValAttentionFullTrainer():
         net = PPG.Models.SnippetConvolutionalTransformer(
             **net_args).to(self.device)
         # nn.L1Loss().to(args["device"]) #nn.CrossEntropyLoss().to(args["device"])
-        criterion = torch.nn.L1Loss().to(self.device)
-        # criterion = torch.nn.MSELoss().to(self.device)
+        #criterion = torch.nn.L1Loss().to(self.device)
+        criterion = torch.nn.MSELoss().to(self.device)
         optimizer = torch.optim.Adam(net.parameters(), lr=lr,
                                      weight_decay=weight_decay)
 
