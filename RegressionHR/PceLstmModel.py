8737e8c280cad81d51695784b58a22976a96b881
Davi Pedrosa de Aguiar <daviaguiar@outlook.com>
2020-11-30 18:34:13 -0300

    f

--- RegressionHR/PceLstmModel.py
+++ RegressionHR/PceLstmModel.py
@@ -325,6 +325,41 @@ class PceDiscriminatorAssembler(torch.nn.Module):
     return self.discriminator(torch.cat([pce0, pce1], axis=1))
 
 
+class PceDiscriminatorAssembler2(torch.nn.Module):
+  def __init__(self, ts_encoder, is_encoder, discriminator):
+    super(PceDiscriminatorAssembler2, self).__init__()
+    self.ts_encoder = ts_encoder
+    self.is_encoder = is_encoder
+    self.discriminator = discriminator
+  
+  def ts_encode(self, x):
+    return torch.cat(
+        [self.ts_encoder(b).transpose(0,2).transpose(1,2)
+          for b in x], dim=0).squeeze(-1)
+  
+  def is_encode(self, encx, y):
+        return self.is_encoder(torch.cat([encx, y], dim=2).transpose(2,1)).squeeze(-1)
+  
+  def compute_pce(self, x, y):
+    enc = self.ts_encode(x)
+    return self.is_encode(enc, y)
+        
+  def forward(self, x0, hr0, x1, hr1):
+    
+      
+    # x0enc = self.ts_encoder(x0.transpose(1,2).reshape(x0.shape[0], x0.shape[2],  -1))
+    # x1enc = self.ts_encoder(x1.transpose(1,2).reshape(x1.shape[0], x1.shape[2],  -1))
+
+    # pce0 = self.is_encoder(torch.cat([x0enc, hr0.transpose(2,1)], axis=1)).squeeze(2)
+    # pce1 = self.is_encoder(torch.cat([x1enc, hr1.transpose(2,1)], axis=1)).squeeze(2) 
+
+    pce0 = self.compute_pce(x0, hr0)
+    pce1 = self.compute_pce(x1, hr1)
+
+    return self.discriminator(torch.cat([pce0, pce1], axis=1))
+
+
+
 def make_pce_lstm_and_discriminator(
     ts_h_size = 32,
     lstm_size=32,
@@ -359,5 +394,5 @@ def parametrized_encoder_make_pce_lstm_and_discriminator(
   pce_lstm = ParametrizedEncoderMakeOurConvLSTM(sample_per_ts, ts_per_is, ts_h_size, is_h_size, lstm_size, lstm_input, dropout_rate, nattrs)()
   discriminator = Discriminator(lstm_size*2, disc_nlayers, disc_layer_size, disc_dropout_rate)
 
-  pce_discriminator = PceDiscriminatorAssembler(pce_lstm.ts_encoder, pce_lstm.is_encoder, discriminator)
+  pce_discriminator = PceDiscriminatorAssembler2(pce_lstm.ts_encoder, pce_lstm.is_encoder, discriminator)
   return pce_lstm, pce_discriminator
