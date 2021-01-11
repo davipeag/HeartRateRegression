bcff4d268647471750593c8386ad2765c6c54094
Davi Pedrosa de Aguiar <daviaguiar@outlook.com>
2021-01-11 17:44:03 -0300

    added fft

--- PPG/NoHrPceLstmModel.py
+++ PPG/NoHrPceLstmModel.py
@@ -154,51 +154,51 @@ class HiddenInitializationConvLSTMAssemblerSeparateBVP(torch.nn.Module):
 
         return ps
 
-# class HiddenInitializationConvLSTMAssemblerAddedFFT(HiddenInitializationConvLSTMAssembler):
-#     def __init__(self, ts_encoder, ts_encoder_bvp, is_encoder, h0_fc_net, c0_fc_net,
-#                  lstm, fc_net, predictor, bvp_idx=4):
+class HiddenInitializationConvLSTMAssemblerAddedFFT(HiddenInitializationConvLSTMAssembler):
+    def __init__(self, ts_encoder, ts_encoder_bvp, is_encoder, h0_fc_net, c0_fc_net,
+                 lstm, fc_net, predictor, bvp_idx=5):
         
-#         kargs = locals()
-#         kargs.pop(self)
-#         super().__init__(**kargs)
+       
+        super().__init__(ts_encoder=ts_encoder, ts_encoder_bvp=ts_encoder_bvp, is_encoder=is_encoder, h0_fc_net=h0_fc_net, c0_fc_net = c0_fc_net,
+                 lstm = lstm, fc_net=fc_net, predictor=predictor, bvp_idx=bvp_idx)
     
-#     def forward(self, xi, yi, xp):
+    def forward(self, xi, yi, xp):
 
-#         xi[:,:,0,:] = 0
-#         xp[:,:,0,:] = 0
+        xi[:,:,0,:] = 0
+        xp[:,:,0,:] = 0
         
-#         xi_bvp = xi[:, :, self.bvp_idx: self.bvp_idx+1, :]
-#         xp_bvp = xp[:, :, self.bvp_idx: self.bvp_idx+1, :]
+        xi_bvp = xi[:, :, self.bvp_idx: self.bvp_idx+1, :]
+        xp_bvp = xp[:, :, self.bvp_idx: self.bvp_idx+1, :]
 
-#         xi_imu = xi[:, :, :self.bvp_idx, :]
-#         xp_imu = xp[:, :, :self.bvp_idx, :]
+        xi_imu = xi[:, :, :self.bvp_idx, :]
+        xp_imu = xp[:, :, :self.bvp_idx, :]
         
-#         encoded_xp_imu = torch.cat(
-#             [self.ts_encoder(b).transpose(0, 2).transpose(1, 2)
-#              for b in xp_imu], dim=0)
+        encoded_xp_imu = torch.cat(
+            [self.ts_encoder(b).transpose(0, 2).transpose(1, 2)
+             for b in xp_imu], dim=0)
 
-#         encoded_xp_bvp = torch.cat(
-#             [self.ts_encoder_bvp(b).transpose(0, 2).transpose(1, 2)
-#              for b in xp_bvp], dim=0)
+        encoded_xp_bvp = torch.cat(
+            [self.ts_encoder_bvp(b).transpose(0, 2).transpose(1, 2)
+             for b in xp_bvp], dim=0)
 
-#         encoded_xp = torch.cat((encoded_xp_bvp, encoded_xp_imu), axis=-1)
+        encoded_xp = torch.cat((encoded_xp_bvp, encoded_xp_imu), axis=-1)
 
-#         xin = xi_imu.transpose(1, 2).reshape(xi_imu.shape[0], xi_imu.shape[2],  -1)
-#         xin_bvp = xi_bvp.transpose(1, 2).reshape(
-#             xi_bvp.shape[0], xi_bvp.shape[2],  -1)
+        xin = xi_imu.transpose(1, 2).reshape(xi_imu.shape[0], xi_imu.shape[2],  -1)
+        xin_bvp = xi_bvp.transpose(1, 2).reshape(
+            xi_bvp.shape[0], xi_bvp.shape[2],  -1)
 
-#         encoded_xi = self.ts_encoder(xin)
-#         encoded_xi_bvp = self.ts_encoder_bvp(xin_bvp)
+        encoded_xi = self.ts_encoder(xin)
+        encoded_xi_bvp = self.ts_encoder_bvp(xin_bvp)
 
-#         ie = torch.cat([encoded_xi, encoded_xi_bvp], axis=1)
+        ie = torch.cat([encoded_xi, encoded_xi_bvp], axis=1)
 
-#         i_enc = self.is_encoder(ie).reshape(xi_imu.shape[0], -1)
-#         h = self.h0_fc_net(i_enc)
-#         c = self.c0_fc_net(i_enc)
-#         hsf, _ = self.lstm(encoded_xp, (h.unsqueeze(0), c.unsqueeze(0)))
-#         ps = self.predictor(self.fc_net(hsf))
+        i_enc = self.is_encoder(ie).reshape(xi_imu.shape[0], -1)
+        h = self.h0_fc_net(i_enc)
+        c = self.c0_fc_net(i_enc)
+        hsf, _ = self.lstm(encoded_xp, (h.unsqueeze(0), c.unsqueeze(0)))
+        ps = self.predictor(self.fc_net(hsf))
 
-#         return ps
+        return ps
 
     
 
@@ -281,6 +281,27 @@ class PpgParametrizedEncoderMakeOurConvLSTMSeparateBVP(PpgParametrizedEncoderMak
     )
     return net
 
+class PpgParametrizedEncoderMakeOurConvLSTMAddedFFT(PpgParametrizedEncoderMakeOurConvLSTM):
+  def __init__(self, input_length=400, ts_per_is=2, ts_h_size=32, is_h_size=32, lstm_size=32, lstm_input=128, dropout_rate=0,
+               nattrs=5, bvp_count=12):
+    
+      super().__init__(input_length=input_length, ts_per_is=ts_per_is, ts_h_size=ts_h_size, is_h_size=is_h_size, lstm_size=lstm_size, lstm_input=lstm_size, dropout_rate=dropout_rate,
+               nattrs=nattrs, bvp_count=bvp_count)
+
+  def __call__(self):
+    net = HiddenInitializationConvLSTMAssemblerAddedFFT (
+    ts_encoder= self.make_ts_encoder(self.nattrs, self.lstm_input - self.bvp_count, self.ts_h_size),
+    ts_encoder_bvp = self.make_ts_encoder(1, self.bvp_count, self.ts_h_size),
+    is_encoder = self.make_is_encoder(),
+    h0_fc_net = self.make_h0_fc_net(),
+    c0_fc_net = self.make_c0_fc_net(),
+    lstm= self.make_lstm(),
+    fc_net = self.make_fc_net(),
+    predictor = self.make_hr_predictor(),
+    bvp_idx=5
+    )
+    return net
+
 
 def ppg_make_par_enc_pce_lstm(
     sample_per_ts = 400,
@@ -321,6 +342,31 @@ def ppg_make_par_enc_pce_lstm_separate_bvp(
   #print(f"ts_per_is: {ts_per_is}")
   #print(f"sample_per_ts: {sample_per_ts}")
   return PpgParametrizedEncoderMakeOurConvLSTMSeparateBVP(
+    input_length=sample_per_ts,
+    ts_per_is=ts_per_is,
+    ts_h_size=ts_h_size,
+    is_h_size=is_h_size,
+    lstm_size=lstm_size,
+    lstm_input=lstm_input,
+    dropout_rate=dropout_rate,
+    nattrs=nattrs,
+    bvp_count=bvp_count)()
+
+
+def ppg_make_par_enc_pce_lstm_added_fft(
+    sample_per_ts = 400,
+    ts_per_is = 2,
+    ts_h_size = 32,
+    is_h_size = 32,
+    lstm_size=32,
+    lstm_input = 128,
+    dropout_rate = 0,
+    nattrs=5,
+    bvp_count = 12
+    ):
+  #print(f"ts_per_is: {ts_per_is}")
+  #print(f"sample_per_ts: {sample_per_ts}")
+  return PpgParametrizedEncoderMakeOurConvLSTMAddedFFT(
     input_length=sample_per_ts,
     ts_per_is=ts_per_is,
     ts_h_size=ts_h_size,
